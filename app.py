33e009cd21aab7437717fd65c4f7b5c22795f9e1
EdemGold <ekmedm@gmail.com>
2022-12-07 17:23:43 -0800

    Addding code update

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("echo")
-x = parser.parse_args()
+parser.add_argument("square", type=int, help="display a square of a given number")
+parser.add_argument("--v", "--verbosity", action="store_true", help="Increase output verbosity")
+args = parser.parse_args()
+answer = args.square**2
 
-print(x.echo)
-
+if args.v:
+	print(answer)
+else:
+	print(answer)
